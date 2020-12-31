bb2f46cf22fe9c19538e272e715e8150738afbaa
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2020-12-31 10:31:50 +0300

    reformat file

--- Урок 1. Практическое задание/task_1.py
+++ Урок 1. Практическое задание/task_1.py
@@ -42,10 +42,10 @@ def check_2(lst_obj):
 
     Сложность: O(N^2).
     """
-    for j in range(len(lst_obj)):          # O(N)
-        if lst_obj[j] in lst_obj[j+1:]:    # O(N) + O(N) = O(N)
-            return False                   # O(1)
-    return True                            # O(1)
+    for j in range(len(lst_obj)):  # O(N)
+        if lst_obj[j] in lst_obj[j + 1:]:  # O(N) + O(N) = O(N)
+            return False  # O(1)
+    return True  # O(1)
 
 
 #############################################################################################
@@ -58,12 +58,13 @@ def check_3(lst_obj):
 
     Сложность: O(NlogN)
     """
-    lst_copy = list(lst_obj)                 # O(N)
-    lst_copy.sort()                          # O(NlogN)
-    for i in range(len(lst_obj) - 1):        # O(N) + O(1)
-        if lst_copy[i] == lst_copy[i+1]:     # O(1)
-            return False                     # O(1)
-    return True                              # O(1)
+    lst_copy = list(lst_obj)  # O(N)
+    lst_copy.sort()  # O(NlogN)
+    for i in range(len(lst_obj) - 1):  # O(N) + O(1)
+        if lst_copy[i] == lst_copy[i + 1]:  # O(1)
+            return False  # O(1)
+    return True  # O(1)
+
 
 #############################################################################################
 
